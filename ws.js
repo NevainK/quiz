4aeb764991985e6a9f488452b6be341ccd2d8620
NevainK <nevain556@gmail.com>
2026-09-06 22:43:28 +0800

    second

--- ws.js
+++ ws.js
@@ -1,33 +1,233 @@
-const CACHE_NAME = "quiz-v1";
+const CACHE_NAME =
+    "quiz-cache-v1";
 
-const FILES = [
+
+const STATIC_FILES = [
+
     "./",
+
     "./index.html",
-    "./manifest.json"
+
+    "./banks.json",
+
+    "./manifest.json",
+
+    "./icons/icon-192.png",
+
+    "./icons/icon-512.png"
+
 ];
 
-self.addEventListener("install", event => {
 
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(cache => cache.addAll(FILES))
-    );
+/*
+ * 安装
+ */
 
-});
+self.addEventListener(
+
+    "install",
+
+    event => {
 
 
-self.addEventListener("fetch", event => {
+        event.waitUntil(
 
-    event.respondWith(
+            caches
+                .open(
+                    CACHE_NAME
+                )
 
-        caches.match(event.request)
-        .then(response => {
+                .then(
 
-            return response ||
-                   fetch(event.request);
+                    cache =>
+                        cache.addAll(
+                            STATIC_FILES
+                        )
 
-        })
+                )
 
-    );
+        );
 
-});
+
+        self.skipWaiting();
+
+    }
+
+);
+
+
+/*
+ * 激活
+ *
+ * 删除旧版本缓存
+ */
+
+self.addEventListener(
+
+    "activate",
+
+    event => {
+
+
+        event.waitUntil(
+
+            caches
+                .keys()
+
+                .then(
+
+                    keys =>
+
+                        Promise.all(
+
+                            keys.map(
+
+                                key => {
+
+                                    if (
+                                        key !==
+                                        CACHE_NAME
+                                    ) {
+
+                                        return caches
+                                            .delete(
+                                                key
+                                            );
+
+                                    }
+
+                                }
+
+                            )
+
+                        )
+
+                )
+
+        );
+
+
+        self.clients.claim();
+
+    }
+
+);
+
+
+/*
+ * 网络请求
+ *
+ * 优先访问网络
+ *
+ * 网络失败时使用缓存
+ */
+
+self.addEventListener(
+
+    "fetch",
+
+    event => {
+
+
+        /*
+         * 只处理 GET
+         */
+
+        if (
+            event.request.method
+            !== "GET"
+        ) {
+
+            return;
+
+        }
+
+
+        event.respondWith(
+
+
+            fetch(
+                event.request
+            )
+
+
+            .then(
+
+                response => {
+
+
+                    /*
+                     * 无效响应不缓存
+                     */
+
+                    if (
+                        !response ||
+                        response.status !== 200
+                    ) {
+
+                        return response;
+
+                    }
+
+
+                    /*
+                     * 克隆
+                     */
+
+                    const clonedResponse =
+                        response.clone();
+
+
+                    /*
+                     * 保存到缓存
+                     */
+
+                    caches
+                        .open(
+                            CACHE_NAME
+                        )
+
+                        .then(
+
+                            cache => {
+
+                                cache.put(
+
+                                    event.request,
+
+                                    clonedResponse
+
+                                );
+
+                            }
+
+                        );
+
+
+                    return response;
+
+                }
+
+            )
+
+
+            /*
+             * 网络失败
+             */
+
+            .catch(
+
+                () =>
+
+                    caches.match(
+                        event.request
+                    )
+
+            )
+
+
+        );
+
+    }
+
+);
